a77c6e4189f5b157876c326f1e19e0e60aacfbee
Lajos Koszti <ajnasz@ajnasz.hu>
2009-10-23 00:17:41 +0200

    basic functionallity
    Replacing of the specified element and
    edit/save/cancel buttons also works

--- src/inlineeditor/js/InlineEditor.js
+++ src/inlineeditor/js/InlineEditor.js
@@ -86,7 +86,16 @@
             * @static
             * @final
             */
-            TYPE: 'text'
+            TYPE: 'text',
+            FIELD_NAME: 'field',
+            FIELD_GENERATOR: function() {
+                var field;
+                switch(this.get('type')) {
+                    case 'text':
+                        field = genTextField(this.get('fieldName'), this.get('value'));
+                }
+                return field;
+            }
         },
         /**
          * Validates the type of the editor
@@ -106,130 +115,272 @@
                 }
             }
             return valid;
+        },
+        createForm = function(id) {
+            var form = document.createElement('form');
+            Dom.setAttribute(form, 'id', 'yui-inline-editor-' + id);
+            Dom.addClass(form, 'yui-inline-editor-form');
+            Event.on(form, 'submit', function(e) {
+                $E.stopEvent(e);
+            });
+            return form;
+        },
+        genTextField = function(name, value) {
+            var element = document.createElement('input');
+            Dom.setAttribute(element, 'name', name);
+            Dom.setAttribute(element, 'value', value);
+            return element;
+        },
+        getFormValues = function(form) {
+            var values,
+                elements;
+            if(form && form.nodeName == 'FORM') {
+                elements = form.elements;
+                values = {};
+                for (var i = 0, el = elements.length, elem, name, value; i < el; i++) {
+                    elem = elements[i];
+                    name = Dom.getAttribute(elem, 'name');
+                    if(name) {
+                        values[name] = elem.value;
+                    }
+                }
+            }
+            return values;
         };
 
 
 
-        YAHOO.extend(InlineEditor, YAHOO.util.AttributeProvider, {
-            save: function() {
-                this.fireEvent(saveEvent);
-            },
-            cancel: function() {
-                this.fireEvent(cancelEvent);
-            },
-            edit: function() {
-                this.fireEvent(editStartedEvent);
-            },
-            _destroyControls: function() {
-                var controls = this.controls,
-                    cancel,
-                    cancelParent,
-                    save,
-                    saveParent,
-                    container,
-                    containerParent;
+    YAHOO.extend(InlineEditor, YAHOO.util.AttributeProvider, {
+        save: function() {
+            var values = getFormValues(this._editor);
+            this.set('value', values[this.get('fieldName')]);
+            this._stopEdit();
+            this.fireEvent(saveEvent);
+        },
+        cancel: function() {
+            this._stopEdit();
+            this.fireEvent(cancelEvent);
+        },
+        edit: function() {
+            this._startEdit();
+            this.fireEvent(editStartedEvent);
+        },
+        _startEdit: function() {
+            var element = this.get('element');
+            Dom.addClass(element, CLASSES.EDITING_ACTIVE);
+            this._replaceElement();
+        },
+        _stopEdit: function() {
+            var element = this.get('element');
+            Dom.removeClass(element, CLASSES.EDITING_ACTIVE);
+            this._restoreElement();
+        },
+        _setEditable: function() {
+            var element = this.get('element');
+            Dom.addClass(element, CLASSES.ELEM_EDITABLE);
+        },
+        _createEditor: function() {
+            var form = createForm(this.get('id')),
+                type = this.get('type'),
+                generator = this.get('fieldGenerator'),
+                field = generator.call(this);
 
-                if(controls) {
-                    container = controls.container;
-                    containerParent = container.parentNode;
-                    if(container) {
-                        containerParent.removeChild(container);
-                    } else {
-                        cancel = controls.cancel;
-                        cancelParent = cancel.parentNode;
-                        save = controls.save;
-                        saveParent = cancel.parentNode;
-                        save = controls.save;
-                        saveParent = cancel.parentNode;
-                        if(cancelParent) {
-                            cancelParent.removeChild(cancel);
-                            Y.log('cancel button removed', 'info');
-                        }
-                        if(saveParent) {
-                            saveParent.removeChild(save);
-                            Y.log('save button removed', 'info');
-                        }
-                    }
-
-                    delete this.controls;
-                    Y.log('controls destroyed', 'info');
-                }
-            },
-            _createControls: function() {
-                var button = document.createElement('button'),
-                    cancelButton = button.cloneNode(false),
-                    saveButton = button.cloneNode(false),
-                    editButton = button.cloneNode(false);
-
-                // if the controls already exists, need to delete them
-                this._destroyControls();
-
-                Y.log('set class');
-                Dom.addClass(cancelButton, CLASSES.CANCEL_BUTTON);
-                Dom.addClass(saveButton, CLASSES.SAVE_BUTTON);
-                Dom.addClass(editButton, CLASSES.EDIT_BUTTON);
-                Y.log('set html');
-                cancelButton.innerHTML = 'cancel';
-                saveButton.innerHTML = 'save';
-                editButton.innerHTML = 'edit';
-                Y.log('set events');
-                Event.on(cancelButton, 'click', function(event) {
-                    this.cancel(event);
-                    this.fireEvent(cancelClickEvent, event);
-                }, this, true);
-                Event.on(saveButton, 'click', function(event) {
-                    this.save(event);
-                    this.fireEvent(saveClickEvent, event);
-                }, this, true);
-                Event.on(editButton, 'click', function(event) {
-                    this.edit(event);
-                    this.fireEvent(editClickEvent, event);
-                }, this, true);
-
-                Y.log('set controls');
-                this.controls = {
-                    cancel: cancelButton,
-                    save: saveButton
-                };
-            },
-            _addControls: function() {
+            if(field.nodeType === 1) {
                 this._createControls();
-
-                var element = this.get('element'),
-                    controlsContainer = document.createElement('span'),
-                    controls = this.controls;
-
-                Dom.addClass(controlsContainer, CLASSES.CONTROLS_CONTAINER);
-
-                controlsContainer.appendChild(controls.cancel);
-                controlsContainer.appendChild(controls.save);
-                element.appendChild(controlsContainer);
-
-                controls.container = controlsContainer;
-                
-            },
-            save: function() {
-            },
-            init: function(el, cfg) {
-                var element = Dom.get(el);
-                if(!element) {
-                    Y.log('Inline Editor element not found', 'error');
-                    return;
-                }
-                cfg = cfg || {};
-                this.setAttributeConfig('element', { value: element });
-                /**
-                 * @attribute type
-                 * The type of the inline editor. If it's not specified
-                 * in the config then the default value is normally 'text'
-                 */
-                this.setAttributeConfig('type', {
-                    validator: validateType,
-                    value: cfg.type || DEFAULT_CONFIG.TYPE
-                });
-
-                this._addControls();
+                form.appendChild(field);
+                form.appendChild(this.controls.cancel);
+                form.appendChild(this.controls.save);
             }
-        });
+            return form;
+        },
+        _replaceElement: function() {
+            var element = this.get('element'),
+                editor = this._createEditor();
+            element.innerHTML = '';
+            element.appendChild(editor);
+            this._editor = editor;
+        },
+        _restoreElement: function() {
+            var element = this.get('element');
+            element.innerHTML = this.get('value');
+            this._addEditControl();
+            delete this._editor;
+        },
 
+
+        _destroyControls: function() {
+            var controls = this.controls,
+                cancel,
+                cancelParent,
+                save,
+                saveParent,
+                container,
+                containerParent;
+
+            if(controls) {
+                container = controls.container;
+                if(container) {
+                    containerParent = container.parentNode;
+                    containerParent.removeChild(container);
+                } else {
+                    cancel = controls.cancel;
+                    cancelParent = cancel.parentNode;
+                    save = controls.save;
+                    saveParent = cancel.parentNode;
+                    save = controls.save;
+                    saveParent = cancel.parentNode;
+                    if(cancelParent) {
+                        cancelParent.removeChild(cancel);
+                        Y.log('cancel button removed', 'info');
+                    }
+                    if(saveParent) {
+                        saveParent.removeChild(save);
+                        Y.log('save button removed', 'info');
+                    }
+                }
+
+                delete this.controls;
+                Y.log('controls destroyed', 'info');
+            }
+        },
+        _createControls: function(type) {
+            var button = document.createElement('button'),
+                cancelButton,
+                saveButton,
+                editButton;
+
+            Dom.setAttribute(button, 'type', 'button');
+            this._destroyControls();
+
+            switch(type) {
+                case 'edit':
+                    editButton = button.cloneNode(false);
+                    Dom.addClass(editButton, CLASSES.EDIT_BUTTON);
+                    editButton.innerHTML = 'edit';
+                    Event.on(editButton, 'click', function(event) {
+                        this.edit(event);
+                        this.fireEvent(editClickEvent, event);
+                    }, this, true);
+                    this.controls = {
+                        edit: editButton
+                    };
+                    break;
+
+                default:
+                    cancelButton = button.cloneNode(false);
+                    saveButton = button.cloneNode(false);
+                    Dom.addClass(cancelButton, CLASSES.CANCEL_BUTTON);
+                    Dom.addClass(saveButton, CLASSES.SAVE_BUTTON);
+                    Y.log('set html');
+                    cancelButton.innerHTML = 'cancel';
+                    saveButton.innerHTML = 'save';
+                    Y.log('set events');
+                    Event.on(cancelButton, 'click', function(event) {
+                        this.cancel(event);
+                        this.fireEvent(cancelClickEvent, event);
+                    }, this, true);
+                    Event.on(saveButton, 'click', function(event) {
+                        this.save(event);
+                        this.fireEvent(saveClickEvent, event);
+                    }, this, true);
+
+                    Y.log('set controls');
+                    this.controls = {
+                        cancel: cancelButton,
+                        save: saveButton,
+                    };
+                    break;
+            }
+        },
+        _addSavingControls: function() {
+            this._createControls();
+
+            var element = this.get('element'),
+                controlsContainer = document.createElement('span'),
+                controls = this.controls;
+
+            Dom.addClass(controlsContainer, CLASSES.CONTROLS_CONTAINER);
+
+            controlsContainer.appendChild(controls.cancel);
+            controlsContainer.appendChild(controls.save);
+            controlsContainer.appendChild(controls.edit);
+            element.appendChild(controlsContainer);
+
+            controls.container = controlsContainer;
+        },
+        _addEditControl: function() {
+            this._createControls('edit');
+
+            var element = this.get('element'),
+                controlsContainer = document.createElement('span'),
+                controls = this.controls;
+
+            Dom.addClass(controlsContainer, CLASSES.CONTROLS_CONTAINER);
+
+            controlsContainer.appendChild(controls.edit);
+            element.appendChild(controlsContainer);
+
+            controls.container = controlsContainer;
+        },
+
+
+        init: function(el, cfg) {
+            var element = Dom.get(el);
+            if(!element) {
+                Y.log('Inline Editor element not found', 'error');
+                return;
+            }
+            cfg = cfg || {};
+            this.setAttributeConfig('id', {
+                value: Dom.generateId(),
+                readOnly: true
+            });
+            /**
+             *
+             */
+            this.setAttributeConfig('element', {
+                value: element,
+                readOnly: true
+            });
+            /**
+             * @attribute type
+             * The type of the inline editor. If it's not specified
+             * in the config then the default value is normally 'text'
+             */
+            this.setAttributeConfig('type', {
+                validator: validateType,
+                value: cfg.type || DEFAULT_CONFIG.TYPE
+            });
+            /**
+             * @attribute fieldName
+             */
+            this.setAttributeConfig('fieldName', {
+                validator: YL.isString,
+                value: cfg.fieldName || DEFAULT_CONFIG.FIELD_NAME
+            });
+            /**
+             * @attribute fieldGenerator
+             */
+            this.setAttributeConfig('fieldGenerator', {
+                validator: YL.isFunction,
+                value: cfg.fieldGenerator || DEFAULT_CONFIG.FIELD_GENERATOR
+            });
+            /**
+             * @attribute displayValue
+             */
+            this.setAttributeConfig('displayValue', {
+                value: cfg.displayValue || element.innerHTML
+            });
+            /**
+             * @attribute value
+             * The current value of the field
+             */
+            this.setAttributeConfig('value', {
+                value: cfg.value || this.get('displayValue')
+            });
+
+            this._addEditControl();
+            this._setEditable();
+        }
+    });
 })();
