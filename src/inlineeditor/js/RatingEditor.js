9be12a56f9f62e4eb5d56e1de6e745bdcdebb715
Lajos Koszti <ajnasz@ajnasz.hu>
2010-05-10 08:57:08 +0200

    add Rating editor widget
    With th rating editor you can create an editor which shows star ratings
    and you can change it

---
+++ src/inlineeditor/js/RatingEditor.js
@@ -0,0 +1,80 @@
+/**
+ * Inline editor widget
+ * @module inlineeditor
+ * @namespace YAHOO.widget
+ */
+(function() {
+  YAHOO.widget.RatingEditor = function(el, cfg) {
+        cfg = YAHOO.lang.merge(cfg, {type: 'rating'});
+        YAHOO.widget.RatingEditor.superclass.constructor.call(this, el, cfg);
+        this._ratingInit.apply(this, arguments);
+  };
+    var Y                   = YAHOO,
+        YU                  = Y.util,
+        YL                  = Y.lang,
+        Event               = YU.Event,
+        Dom                 = YU.Dom,
+        Selector            = YU.Selector,
+
+        fieldGenerator = function(type, fieldName, value) {
+          var fields = '',
+              maxRate = this.get('maxRate'),
+              i = 1;
+
+          while(i <= maxRate) {
+            if(value == i) {
+              fields += '<label class="yui-rate-' + i + '"><input type="radio" name="' + fieldName +'" value="' + i + '" checked="checked"></label>';
+            } else {
+              fields += '<label class="yui-rate-' + i + '"><input type="radio" name="' + fieldName +'" value="' + i + '" /></label>';
+            }
+            i++;
+          }
+          var output = document.createElement('p');
+          Dom.addClass(output, 'yui-rating yui-selected-rate' + value);
+
+          output.innerHTML = fields;
+          return output;
+
+        },
+        htmlFromValue = function(value) {
+          return '<span class="yui-selected-rate' + value + ' yui-rate"><span class="yui-rate-indicator"></span></span>';
+        };
+        YAHOO.extend(YAHOO.widget.RatingEditor, YAHOO.widget.InlineEditor, {
+          _ratingInit: function(cfg) {
+            this.set('fieldGenerator', fieldGenerator);
+            this.setAttributeConfig('maxRate', {
+              validator: YL.isNumber,
+              value: YL.isNumber(cfg.maxRate) ? cfg.maxRate : 5
+            });
+            this.setAttributeConfig('value', {
+              getter: function(name, value) {
+                var output;
+                if(YL.isNumber(value)) {
+                  output = value;
+                } else {
+                  output = +this.get('htmlValue').replace(/\n/g, '').replace(/.*yui-selected-rate(\d+).*/, '$1');
+                }
+                return output;
+              }
+            });
+            this.subscribe('elementReplacedEvent', function() {
+              var ratingEditor = this;
+              var editor = this._editor;
+              var labels = Selector.query('input[type="radio"]', editor);
+
+              Event.on(labels, 'click', function(e) {
+                Y.log('clicked')
+                var p = editor.firstChild;
+                p.className = '';
+                var target = Event.getTarget(e);
+                Dom.addClass(p, 'yui-rating yui-selected-rate' + target.value);
+              });
+            });
+            this.subscribe('elementRestoredEvent', function() {
+                var htmlValue = htmlFromValue(this.get('value'));
+                this.set('htmlValue', htmlValue);
+                this.get('element').innerHTML = htmlValue;
+            });
+          }
+        });
+})();
